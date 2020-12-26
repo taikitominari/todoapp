7df01012830c5e57455a275127f9d580f5ff9914
taikitominari <taikitominari.co.jp@icloud.com>
2020-12-27 03:35:08 +0900

    削除,完了昨日の追加

--- js/index.js
+++ js/index.js
@@ -1,13 +1,37 @@
 "use strict";
 
-let add = document.getElementById('add_button');
+const add = document.getElementById('add_button');
 const list = document.getElementById('list');
+let text = document.getElementById("text");
 
-add.onclick = function() {
-  let text = document.getElementById("text").value;
-  if (text !== "") {
+
+const addTask = add => {
+  if (add !== "") {
     const taskLi = document.createElement('li');
-    taskLi.textContent = text;
+    const removeButton = document.createElement('button');
+    const eraseButton = document.createElement('button');
+
+    removeButton.innerText = '削除';
+    eraseButton.innerText = '完了';
+
+    removeButton.addEventListener('click', a => {
+      const removeTask = removeButton.closest('li');
+      list.removeChild(removeTask);
+    });
+    eraseButton.addEventListener('click', a => {
+      const eraseTask = removeButton.closest('li');
+      taskLi.classList.add('line');
+    });
+
+// liの追加処理
+    taskLi.textContent = add;
+    taskLi.append(removeButton);
+    taskLi.append(eraseButton);
     list.insertBefore(taskLi, list.firstChild);
-  };
-}
+  }
+};
+
+add.addEventListener('click' , () => {
+  const value = text.value;
+  addTask(value);
+});
